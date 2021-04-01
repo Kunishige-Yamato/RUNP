421423eebf351da77b391bec7a8e247b02e526d3
Kunishige-Yamato <tmc20247010@gmail.com>
2021-04-01 17:57:50 +0900

    Commit 1.6
    プレイヤーのジャンプ回数の制限

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject player;
+    public int jump;
 
     void Start()
     {
-        
+        jump=0;
     }
 
     void Update()
     {
-        //入力があったらジャンプ
-        if(Input.anyKeyDown){
-            player.GetComponent<Rigidbody>().useGravity=true;
-            Vector3 speed;
-            speed.x=0;
-            speed.y=8;
-            speed.z=0;
-            player.GetComponent<Rigidbody>().AddForce(speed,ForceMode.Impulse);
+        if(jump==0){
+            //入力があったらジャンプ
+            if(Input.anyKeyDown){
+                jump=1;
+                player.GetComponent<Rigidbody>().useGravity=true;
+                Vector3 speed;
+                speed.x=0;
+                speed.y=8;
+                speed.z=0;
+                player.GetComponent<Rigidbody>().AddForce(speed,ForceMode.Impulse);
+            }
+        }
+        if(jump==1){
+            if(transform.position.y<=0.1){
+                jump=0;
+            }
         }
     }
 }
