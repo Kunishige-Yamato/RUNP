30aff523936cef5a3e912a86dfce2e6ed6a7609a
Kunishige-Yamato <tmc20247010@gmail.com>
2021-04-04 09:48:01 +0900

    Commit 1.7
    タイトルシーンの作成
    スタートボタンの配置
    タイトル背景の配置
    タイトルイメージの配置
    ボタンでのシーン切り替え

--- Assets/Progress.cs
+++ Assets/Progress.cs
@@ -8,20 +8,24 @@ public class Progress : MonoBehaviour
     public GameObject wallPrefab;
     //タイマー
     public int timer;
+    //壁と壁の間隔用変数
     public int ran;
 
     void Start()
     {
         timer=0;
-        ran=Random.Range(900,1200);
+        //壁同士の間隔の設定
+        ran=Random.Range(600,1200);
     }
 
     void Update()
     {
         timer++;
         if(timer%ran==0){
+            //間隔の再設定
             ran=Random.Range(600,1200);
             timer=0;
+            //壁生成
             GameObject wall=GameObject.Instantiate(wallPrefab);
         }
     }
---
+++ Assets/StartGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+        SceneManager.LoadScene("Game");
+    }
+}
